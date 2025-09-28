01abe07ffc1dcaa69df8d525ef6c38c1d7e0cefa
Pepijn <145846855+DaRealPSL@users.noreply.github.com>
2025-09-28 23:31:12 +0200

    Update cli.h

--- include/cli.h
+++ include/cli.h
@@ -1,7 +1,22 @@
 #ifndef CLI_H
 #define CLI_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void cli_loop(void);
 void print_banner(void);
+void cli_init(void);
+void cli_cleanup(void);
+void cli_set_key(const char *key);
+const char *cli_get_key(void);
+void cli_set_cipher(const char *name);
+const char *cli_get_cipher(void);
+int cli_execute(const char *line);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
